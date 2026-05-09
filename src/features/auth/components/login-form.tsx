1244f5d210725acc28d1ca51758b3b44d4c85b7e
dharma herz <126298627+dexart15@users.noreply.github.com>
2026-05-09 22:14:46 +0800

    fix content login and register

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -69,9 +69,9 @@ export function LoginForm({
           <form className="p-6 md:p-8" onSubmit={handleSubmit}>
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <h1 className="text-2xl font-bold">Selamat Datang Kembali</h1>
                 <p className="text-balance text-muted-foreground">
-                  Login ke akun TekniSini
+                  Masuk ke akun TekniSini
                 </p>
               </div>
               <FormInput
@@ -107,12 +107,12 @@ export function LoginForm({
 
               <Field>
                 <Button disabled={mutation.isPending} type="submit">
-                  {mutation.isPending ? <Spinner /> : "Login"}
+                  {mutation.isPending ? <Spinner /> : "Masuk"}
                 </Button>
               </Field>
 
               <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-                Or continue with
+                Atau lanjutkan dengan
               </FieldSeparator>
               <Field className="grid grid-cols-1 gap-4">
                 <Button variant="outline" type="button" onClick={onGoogleLogin}>
@@ -122,18 +122,18 @@ export function LoginForm({
                       fill="currentColor"
                     />
                   </svg>
-                  Login with Google
+                  Masuk dengan Google
                 </Button>
               </Field>
 
               <FieldDescription className="text-center">
-                Don&apos;t have an account? <a href="/register">Sign up</a>
+                Belum punya akun? <a href="/register">Daftar disini</a>
               </FieldDescription>
             </FieldGroup>
           </form>
           <div className="relative hidden bg-muted md:block">
             <img
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+              src="/images/image-login.jpg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
@@ -141,8 +141,8 @@ export function LoginForm({
         </CardContent>
       </Card>
       <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        Dengan mengeklik lanjutkan, Anda menyetujui <a href="#">Ketentuan Layanan</a>{" "}
+        dan <a href="#">Kebijakan Privasi</a> kami.
       </FieldDescription>
     </div>
   );
